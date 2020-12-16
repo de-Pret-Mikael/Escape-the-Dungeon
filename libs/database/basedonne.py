3e62ca8cdc055078813c02c7793a0131101cb1d2
cyrilGrandjean <64021617+cyrilGrandjean@users.noreply.github.com>
2020-12-16 21:34:39 +0100

    update de la base de donnée et de la class data

--- libs/database/basedonne.py
+++ libs/database/basedonne.py
@@ -3,11 +3,19 @@ import sqlite3
 
 class Data:
     def __init__(self, path):
-        self.path = path
+        self.__path = path
+
+    @property
+    def path(self):
+        return self.__path
 
     def connect(self):
         self.connection = sqlite3.connect(self.path)
         self.cursor = self.connection.cursor()
+
+    def close(self):
+        self.cursor.close()
+        self.connection.close()
 
     def open_sql(self, path):
         try:
@@ -23,10 +31,12 @@ class Data:
 
     def execute(self, action):
         self.cursor.execute(action)
+        self.connection.commit()
 
     def insert(self, table, nameRows, value):
         value = ["'" + i + "'" for i in value]
         value = ", ".join(value)
+        print(value)
         action = "INSERT INTO {}({}) VALUES ({})".format(table, nameRows, value)
         self.execute(action)
 
@@ -34,7 +44,5 @@ class Data:
 if __name__ == '__main__':
     db = Data("escape-the-donjon.db")
     db.connect()
-    db.execute("INSERT INTO Player(nom) VALUES ('test')")
     db.insert("Player", "nom", ["test123"])
-    db.cursor.close()
-    db.connection.close()
+    db.close()
