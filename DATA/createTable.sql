e18723a5b01e24b7f8f2bc2ba15b036683751c5e
cyrilGrandjean <64021617+cyrilGrandjean@users.noreply.github.com>
2020-12-17 19:03:15 +0100

    Update createTable.sql
    ajoute des ligne pour la creation de la table mobs

--- DATA/createTable.sql
+++ DATA/createTable.sql
@@ -1,12 +1,22 @@
-CREATE TABLE "Player" (
-	"id"	INTEGER UNIQUE,
-	"nom"	TEXT,
-	"score"	INTEGER,
-	PRIMARY KEY("id")
+CREATE TABLE "Player"
+(
+    "id"    INTEGER UNIQUE,
+    "nom"   TEXT,
+    "score" INTEGER,
+    PRIMARY KEY ("id")
 );
 
-CREATE TABLE "Item" (
-	"score"	INTEGER,
-	"item"	INTEGER UNIQUE,
-	PRIMARY KEY("item")
+CREATE TABLE "Item"
+(
+    "score" INTEGER,
+    "item"  INTEGER UNIQUE,
+    PRIMARY KEY ("item")
+);
+
+CREATE TABLE "mobs"
+(
+    "nom"     char(10),
+    "couleur" char(10),
+    "vie"     INTEGER,
+    PRIMARY KEY ("nom", "couleur")
 );
